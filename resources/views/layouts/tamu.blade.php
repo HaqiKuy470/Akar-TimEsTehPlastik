4168e4f3af2e15c21900183cf97a3ef6c9e30be5
haqikuy470 <haqikuy470@gmail.com>
2026-09-01 10:57:24 +0700

    Tagline 'Analisis Kausal dan Rekomendasi': Fraunces italic + pemisah tipis
    - Tambah varian italic Fraunces ke Google Fonts
    - Utility .merek-tagline (serif miring)
    - Halaman masuk: wordmark | garis pemisah | tagline serif miring

--- resources/views/layouts/tamu.blade.php
+++ resources/views/layouts/tamu.blade.php
@@ -7,17 +7,18 @@
     <link rel="icon" href="{{ asset('logo.svg') }}" type="image/svg+xml">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,500;9..144,600;9..144,700&family=Inter:wght@400;450;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght@0,9..144,500;0,9..144,600;0,9..144,700;1,9..144,400;1,9..144,500&family=Inter:wght@400;450;500;600;700&display=swap" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
 </head>
 <body class="grid min-h-full lg:grid-cols-[1fr_460px]">
     {{-- Kolom kiri: pernyataan produk, hanya di layar lebar. --}}
     <aside class="relative hidden flex-col justify-between bg-navy-900 p-12 text-white lg:flex">
-        <div class="flex items-center gap-3">
+        <div class="flex items-center gap-3.5">
             <img src="{{ asset('logo.svg') }}" alt="" class="size-11 shrink-0">
-            <span class="merek text-[24px]" style="color: var(--color-krem-merek)">AKAR</span>
-            <span class="text-[12px] text-biru-300">Analisis Kausal dan Rekomendasi</span>
+            <span class="merek text-[25px]" style="color: var(--color-krem-merek)">AKAR</span>
+            <span class="h-6 w-px bg-white/20" aria-hidden="true"></span>
+            <span class="merek-tagline text-[15px] text-biru-300">Analisis Kausal dan Rekomendasi</span>
         </div>
 
         <div class="max-w-md">
@@ -47,7 +48,7 @@
                 <img src="{{ asset('logo.svg') }}" alt="" class="size-10">
                 <div>
                     <div class="merek text-[22px] text-navy-900">AKAR</div>
-                    <p class="text-[11px] text-teks-500">Analisis Kausal dan Rekomendasi</p>
+                    <p class="merek-tagline text-[12px] text-teks-500">Analisis Kausal dan Rekomendasi</p>
                 </div>
             </div>
 
